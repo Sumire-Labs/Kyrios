604ce45c14d987eda7c5c6ebf5778c6d909fe453
s12kuma01 <amukat1201.ta@gmail.com>
2025-09-24 22:57:47 +0900

    Create translation directory structure

---
+++ translation/__init__.py
@@ -0,0 +1,12 @@
+# Translation module for Luna bot
+
+from .deepl_extractor import DeepLExtractor
+from .translation_service import TranslationService
+from .constants import LanguageCodes, TranslationUI
+
+__all__ = [
+    'DeepLExtractor',
+    'TranslationService',
+    'LanguageCodes',
+    'TranslationUI'
+]
